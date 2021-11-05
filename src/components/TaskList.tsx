9ef283c114628684006a40c9ba1d9213f6bb222a
deivaotv <xdeividsabinox@live.com>
2021-11-05 00:29:18 -0300

    feat: função de remover uma nova tarefa

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -35,6 +35,8 @@ export function TaskList() {
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newListTasks = tasks.filter(task => task.id !== id);
+    setTasks(newListTasks);
   }
 
   return (
